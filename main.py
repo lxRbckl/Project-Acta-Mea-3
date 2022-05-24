db8fb66bb22d1adcd72ab961c356bc4f3135324e
ala2q6 <lxrbckl@protonmail.com>
2022-05-24 18:05:54 -0500

    initial commit

--- main.py
+++ main.py
@@ -10,6 +10,15 @@
 # >
 
 
+# set (a server or service)
+# get (a server or default all keys)
+# update (a server)
+# delete (a server or service)
+
+
+
+
+
 # main <
 if (__name__ == '__main__'): pass
 
