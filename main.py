efba7a3dc7dd75d7123db5f5b304635c299be966
ala2q6 <lxrbckl@protonmail.com>
2022-07-18 22:53:56 -0500

    version update to abide by lxRbckl package version

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+# Project Acta Mea by Alex Arbuckle #
+
+
 # import <
 from os import path
 from json import loads
 from github import Github
 from discord import Intents
 from discord.ext import commands
-from lxRbckl import jsonLoad, jsonDump
+from lxRbckl import githubGet, githubSet, jsonLoad
 
 # >
 
@@ -21,50 +24,25 @@ discordToken = ''
 # >
 
 
-def setData(pData: dict, pGithub = Github(githubToken)) -> None:
-    '''  '''
-
-    # get content from file in repository <
-    # update file <
-    repository = pGithub.get_repo(gRepository)
-    content = repository.get_contents(gFile)
-    repository.update_file(
-
-        sha = content.sha,
-        message = 'update',
-        path = content.path,
-        content = str(pData).replace('\'', '\"')
-
-    )
-
-    # >
-
-
-def getData(pGithub = Github(githubToken)) -> dict:
-    '''  '''
-
-    # get data <
-    # try if structured return decoded <
-    # except then unstructured return decoded <
-    content = pGithub.get_repo(gRepository).get_contents(gFile)
-    try: return loads(content.decoded_content.decode())
-    except: return dict(content.decoded_content.decode())
-
-    # >
-
-
 async def setFunction(ctx, pServer: str, pService: str, pData: dict):
     '''  '''
 
-    # if server <
-    # elif service <
+    # if (server) <
+    # elif (service) <
     if ((pServer not in pData.keys()) and (not pService)):
 
         # add new element <
         # set data and delete message <
         pData[pServer] = []
-        setData(pData = pData)
         await ctx.message.delete()
+        githubSet(
+
+            pFile = gFile,
+            pData = pData,
+            pRepository = gRepository,
+            pGithub = Github(githubToken)
+
+        )
 
         # >
 
@@ -73,8 +51,15 @@ async def setFunction(ctx, pServer: str, pService: str, pData: dict):
         # add service to server <
         # set data and delete message <
         pData[pServer].append(pService)
-        setData(pData = pData)
         await ctx.message.delete()
+        githubSet(
+
+            pFile = gFile,
+            pData = pData,
+            pRepository = gRepository,
+            pGithub = Github(githubToken)
+
+        )
 
         # >
 
@@ -88,19 +73,13 @@ async def getFunction(ctx, pServer: str, pService: str, pData: dict):
     # elif default <
     if (pServer in pData.keys()):
 
-        # send output and delete message <
         await ctx.channel.send('\n'.join(f'`{i}`' for i in pData[pServer]), delete_after = 60)
         await ctx.message.delete()
 
-        # >
-
     elif (not pServer):
 
-        # send output and delete message <
         await ctx.channel.send('\n'.join(f'`{i}`' for i in pData.keys()), delete_after = 60)
         await ctx.message.delete()
-
-        # >
 
     # >
 
@@ -114,14 +93,16 @@ async def updateFunction(ctx, pServerTo: str, pServerFrom: str, pData: dict):
         # add new server <
         # delete old server <
         pData[pServerTo] = pData[pServerFrom]
-        del pData[pServerFrom]
-
-        # >
-
-        # set data <
-        # delete message <
-        setData(pData = pData)
         await ctx.message.delete()
+        del pData[pServerFrom]
+        githubSet(
+
+            pFile = gFile,
+            pData = pData,
+            pRepository = gRepository,
+            pGithub = Github(githubToken)
+
+        )
 
         # >
 
@@ -135,23 +116,29 @@ async def deleteFunction(ctx, pServer: str, pService: str, pData: dict):
     # elif service then delete service <
     if ((pServer in pData.keys()) and (not pService)):
 
-        # delete server <
-        # set data and delete message <
         del pData[pServer]
-        setData(pData = pData)
         await ctx.message.delete()
+        githubSet(
 
-        # >
+            pFile = gFile,
+            pData = pData,
+            pRepository = gRepository,
+            pGithub = Github(githubToken)
+
+        )
 
     elif ((pServer in pData.keys()) and (pService)):
 
-        # delete service from server <
-        # set data and delete message <
         pData[pServer].remove(pService)
-        setData(pData = pData)
         await ctx.message.delete()
+        githubSet(
 
-        # >
+            pFile = gFile,
+            pData = pData,
+            pRepository = gRepository,
+            pGithub = Github(githubToken)
+
+        )
 
     # >
 
